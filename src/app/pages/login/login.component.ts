d666ed356136764094d2967f94442388303e6d46
diego <curbelord@gmail.com>
2024-09-12 17:00:28 +0100

    Guest login and minor fixes

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -12,14 +12,14 @@ import { FormErrorComponent } from '../../shared/components/form-error/form-erro
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  inputTypes: string[] = ["email", "password"];
-  errorMessages: {required: string, pattern: string}[] = [{required: "Email is required", pattern: "The email must be of type name@domain.ext"}, {required: "Password is required", pattern: "The password must have at least: 1 uppercase, 1 lowercase, 1 number and 1 special character ( !@#$%&\/()=?¿¡^*+,.- ). It must be between 8 and 16 digits long."}];
+  inputTypes: string[] = ["nick", "password"];
+  errorMessages: {required: string, pattern: string}[] = [{required: "The nickname is required", pattern: "The nickname must contain between 6 and 18 digits, which can include letters, numbers, hyphens and underscores."}, {required: "Password is required", pattern: "The password must have at least: 1 uppercase, 1 lowercase, 1 number and 1 special character ( !@#$%&\/()=?¿¡^*+,.- ). It must be between 8 and 16 digits long."}];
 
-  emailPattern: RegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  nickPattern: RegExp = /^[A-Za-z0-9_-]{6,18}$/;
   passwordPattern: RegExp = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%&\/()=?¿¡^*+,.-])[A-Za-z\d!@#$%&\/()=?¿¡^*+,.-]{8,16}$/;
 
   loginForm: FormGroup = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.pattern(this.emailPattern)]),
+    nick: new FormControl('', [Validators.required, Validators.pattern(this.nickPattern)]),
     password: new FormControl('', [Validators.required, Validators.pattern(this.passwordPattern)])
   });
 
